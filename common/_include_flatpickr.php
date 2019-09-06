a64a58bb41ff050da1b5ff8e802cc53898ee144a
root <root@oredocenti.marconirovereto.it>
2019-09-06 16:02:55 +0200

    Installa flatpickr con npm

--- common/_include_flatpickr.php
+++ common/_include_flatpickr.php
@@ -2,7 +2,7 @@
 <script type="text/javascript" src="<?php echo $__application_base_path; ?>/common/timejs/date-it-IT.js"></script>
 
 <!-- flatpickr -->
-<script type="text/javascript" src="<?php echo $__application_base_path; ?>/common/flatpickr/dist/flatpickr.min.js"></script>
-<script type="text/javascript" src="<?php echo $__application_base_path; ?>/common/flatpickr/dist/l10n/it.js"></script>
-<link rel="stylesheet" href="<?php echo $__application_base_path; ?>/common/flatpickr/dist/flatpickr.min.css">
-<link rel="stylesheet" href="<?php echo $__application_base_path; ?>/common/flatpickr/dist/themes/material_red.css">
+<script type="text/javascript" src="<?php echo $__application_base_path; ?>/node_modules/flatpickr/dist/flatpickr.min.js"></script>
+<script type="text/javascript" src="<?php echo $__application_base_path; ?>/node_modules/flatpickr/dist/l10n/it.js"></script>
+<link rel="stylesheet" href="<?php echo $__application_base_path; ?>/node_modules/flatpickr/dist/flatpickr.min.css">
+<link rel="stylesheet" href="<?php echo $__application_base_path; ?>/node_modules/flatpickr/dist/themes/material_red.css">
