8c9554b3f26367b12f346d2d9909826068431fb9
Mauro Longano <mauro.longano@marconirovereto.it>
2019-10-11 01:36:03 +0200

    Aggiungi la possibilità di modificare le ore assegnate

---
+++ segreteria/oreAssegnateUpdateDetails.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ *  This file is part of GestOre
+ *  @author     Paolo Scapin <paolo.scapin@gmail.com>
+ *  @copyright  (C) 2018 Paolo Scapin
+ *  @license    GPL-3.0+ <https://www.gnu.org/licenses/gpl-3.0.html>
+ */
+
+require_once '../common/checkSession.php';
+ruoloRichiesto('segreteria-docenti','dirigente');
+
+if(isset($_POST)) {
+	$ore_previste_attivita_id = $_POST['ore_previste_attivita_id'];
+	$dettaglio = $_POST['dettaglio'];
+	$ore = $_POST['ore'];
+
+    $query = "UPDATE ore_previste_attivita SET dettaglio = '$dettaglio', ore = '$ore' WHERE id = $ore_previste_attivita_id";
+	dbExec($query);
+
+	info("aggiornatà attività ore_previste_attivita_id=$ore_previste_attivita_id dettaglio=$dettaglio ore=$ore");
+}
+?>
