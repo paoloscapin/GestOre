232a4064ed12cf6a9e5d6eafbd4d3d3b3b83ea94
Paolo Scapin <paolo.scapin@archifit.com>
2021-10-12 17:48:34 +0200

    aggiunta gestione utente, icone admin

---
+++ admin/utente.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ *  This file is part of GestOre
+ *  @author     Paolo Scapin <paolo.scapin@gmail.com>
+ *  @copyright  (C) 2018 Paolo Scapin
+ *  @license    GPL-3.0+ <https://www.gnu.org/licenses/gpl-3.0.html>
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Materie</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<?php
+require_once '../common/checkSession.php';
+
+require_once '../common/header-common.php';
+require_once '../common/style.php';
+ruoloRichiesto('dirigente');
+?>
+
+<link rel="stylesheet" href="<?php echo $__application_base_path; ?>/css/table-green.css">
+
+<script type="text/javascript" src="js/utente.js"></script>
+</head>
+
+<body >
+<?php require_once '../common/header-admin.php'; ?>
+
+<!-- Content Section -->
+<div class="container-fluid" style="margin-top:60px">
+<div class="panel panel-teal4">
+<div class="panel-heading container-fluid">
+	<div class="row">
+		<div class="col-md-6">
+			<span class="glyphicon glyphicon-user"></span>&emsp;Gestione Utenti
+		</div>
+        <div class="col-md-6">
+            <div class="pull-right">
+				<button class="btn btn-xs btn-teal4" onclick="utenteGetDetails(-1)" ><span class="glyphicon glyphicon-plus"></span></button>
+            </div>
+        </div>
+	</div>
+</div>
+<div class="panel-body">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="records_content"></div>
+        </div>
+    </div>
+</div>
+
+<!-- <div class="panel-footer"></div> -->
+</div>
+
+<!-- Bootstrap Modals -->
+<!-- Modal - Add/Update Record -->
+<div class="modal fade" id="update_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h5 class="modal-title" id="myModalLabel">Utente</h5>
+            </div>
+            <div class="modal-body">
+
+            <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" placeholder="username" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                    <label for="cognome">Cognome</label>
+                    <input type="text" id="cognome" placeholder="cognome" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                    <label for="nome">Nome</label>
+                    <input type="text" id="nome" placeholder="nome" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                    <label for="ruolo">Ruolo</label>
+                    <input type="text" id="ruolo" placeholder="ruolo" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                    <label for="email">email</label>
+                    <input type="text" id="email" placeholder="email" class="form-control"/>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Annulla</button>
+                <button type="button" class="btn btn-primary" onclick="utenteSave()">Salva</button>
+				<input type="hidden" id="hidden_record_id">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- // Modal - Add New Record -->
+
+</body>
+</html>
--- common/header-admin.php
+++ common/header-admin.php
@@ -16,19 +16,21 @@
 
 		<ul class="nav navbar-nav top-navbar-nav">
 
-			<a href="<?php echo $__application_base_path; ?>/dirigente/index.php" class="btn btn-default navbar-btn btn-deeporange4" role="button"><span class="glyphicon glyphicon-list-alt"></span>&ensp;Dirigente </a>
-			<a href="<?php echo $__application_base_path; ?>/segreteria/index.php" class="btn btn-default navbar-btn btn-teal4" role="button"><span class="glyphicon glyphicon-education"></span>&ensp;Segreteria </a>
+			<a href="<?php echo $__application_base_path; ?>/dirigente/index.php" class="btn btn-default navbar-btn btn-deeporange4" role="button"><span class="glyphicon glyphicon-king"></span>&ensp;Dirigente </a>
+			<a href="<?php echo $__application_base_path; ?>/segreteria/index.php" class="btn btn-default navbar-btn btn-teal4" role="button"><span class="glyphicon glyphicon-tower"></span>&ensp;Segreteria </a>
 			<a href="<?php echo $__application_base_path; ?>/dirigente/selezionaDocente.php" class="btn btn-default navbar-btn btn-lightblue4" role="button"><span class="glyphicon glyphicon-education"></span>&ensp;Docente </a>
-			<a href="<?php echo $__application_base_path; ?>/didattica/index.php" class="btn btn-default navbar-btn btn-orange4" role="button"><span class="glyphicon glyphicon-education"></span>&ensp;Didattica </a>
-			<a href="<?php echo $__application_base_path; ?>/admin/materia.php" class="btn btn-default navbar-btn btn-yellow4" role="button"><span class="glyphicon glyphicon-list-alt"></span>&ensp;Materie </a>
-			<a href="<?php echo $__application_base_path; ?>/admin/attivita.php" class="btn btn-default navbar-btn btn-lima4" role="button"><span class="glyphicon glyphicon-list-alt"></span>&ensp;Tipo Attività </a>
+			<a href="<?php echo $__application_base_path; ?>/didattica/index.php" class="btn btn-default navbar-btn btn-orange4" role="button"><span class="glyphicon glyphicon-knight"></span>&ensp;Didattica </a>
+			<a href="<?php echo $__application_base_path; ?>/admin/materia.php" class="btn btn-default navbar-btn btn-yellow4" role="button"><span class="glyphicon glyphicon-compressed"></span>&ensp;Materie </a>
+			<a href="<?php echo $__application_base_path; ?>/admin/attivita.php" class="btn btn-default navbar-btn btn-lima4" role="button"><span class="glyphicon glyphicon-blackboard"></span>&ensp;Tipo Attività </a>
 			<a href="<?php echo $__application_base_path; ?>/admin/fuisAssegnatoTipo.php" class="btn btn-default navbar-btn btn-lima4" role="button"><span class="glyphicon glyphicon-list-alt"></span>&ensp;Tipo FuisAssegnato </a>
-			<a href="<?php echo $__application_base_path; ?>/admin/annoScolastico.php" class="btn btn-default navbar-btn btn-yellow4" role="button"><span class="glyphicon glyphicon-list-alt"></span>&ensp;Anno Scolastico </a>
+			<a href="<?php echo $__application_base_path; ?>/admin/annoScolastico.php" class="btn btn-default navbar-btn btn-yellow4" role="button"><span class="glyphicon glyphicon-calendar"></span>&ensp;Anno Scolastico </a>
+			<a href="<?php echo $__application_base_path; ?>/admin/utente.php" class="btn btn-default navbar-btn btn-teal4" role="button"><span class="glyphicon glyphicon-user"></span>&ensp;Utente </a>
 		</ul>
 		<ul class="nav navbar-nav navbar-right top-navbar-nav">
+		<li><a href="<?php echo $__application_base_path; ?>/help/GestOre - Guida Studenti.pdf" target="_blank" ><span class="glyphicon glyphicon-bishop"></span></a></li>
 		<li><a href="<?php echo $__settings->local->helpLinkDocente; ?>" target="_blank" ><span class="glyphicon glyphicon-education"></span></a></li>
-		<li><a href="<?php echo $__settings->local->helpLinkSegreteria; ?>" target="_blank" ><span class="glyphicon glyphicon-user"></span></a></li>
-		<li><a href="<?php echo $__settings->local->helpLinkDirigente; ?>" target="_blank" ><span class="glyphicon glyphicon-eye-open"></span></a></li>
+		<li><a href="<?php echo $__settings->local->helpLinkSegreteria; ?>" target="_blank" ><span class="glyphicon glyphicon-tower"></span></a></li>
+		<li><a href="<?php echo $__settings->local->helpLinkDirigente; ?>" target="_blank" ><span class="glyphicon glyphicon-king"></span></a></li>
 		<li><a href="<?php echo $__settings->local->helpLinkAdmin; ?>" target="_blank" ><span class="glyphicon glyphicon-text-color"></span></a></li>
 			<li><a><span class=""></span><?php echo $__utente_nome.' '.$__utente_cognome ?></a></li>
 			<li><?php echo '<a href='.$__application_base_path.'/common/logout.php?base=segreteria><span class="glyphicon glyphicon-log-out"></span></a>'; ?></li>
