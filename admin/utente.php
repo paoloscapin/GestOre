e628880b3cce0452a3b4547db766fc0943d5f935
Paolo Scapin <paolo.scapin@archifit.com>
2022-09-27 15:30:27 +0200

    fix titolo sbagliato per utente

--- admin/utente.php
+++ admin/utente.php
@@ -12,7 +12,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Materie</title>
+	<title>Utenti</title>
 	<meta charset="UTF-8">
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <?php
