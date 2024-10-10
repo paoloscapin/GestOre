86632c3fdef47d28c62fbafb4d063c630c00ab20
Massimo Saiani <massimo.saiani@buonarroti.tn.it>
2024-10-10 16:12:05 +0200

    Aggiunto DST ora sportello - aggiornato invio mail studenti e docenti

--- studente/sportelloInviaMailDocente.php
+++ studente/sportelloInviaMailDocente.php
@@ -7,37 +7,44 @@
  *  @license    GPL-3.0+ <https://www.gnu.org/licenses/gpl-3.0.html>
  */
 
-// contiene ID dello sportello a cui lo studente si è appena iscritto    
-//$sportello_id 
+// // contiene ID dello sportello a cui lo studente si è appena iscritto    
+// // $sportello_id 
 
-$query = "SELECT
-        sportello.id as sportello_id,
-        sportello.data as sportello_data,
-        sportello.ora as sportello_ora,
-        sportello.numero_ore as sportello_numero_ore,
-        sportello.classe as sportello_classe,
+// $query = "SELECT
+//         sportello.id as sportello_id,
+//         sportello.data as sportello_data,
+//         sportello.ora as sportello_ora,
+//         sportello.numero_ore as sportello_numero_ore,
+//         sportello.classe as sportello_classe,
 
-        docente.cognome AS docente_cognome,
-        docente.nome AS docente_nome,
-        docente.id AS docente_id,
-        docente.email AS docente_email,
+//         docente.cognome AS docente_cognome,
+//         docente.nome AS docente_nome,
+//         docente.id AS docente_id,
+//         docente.email AS docente_email,
 
-        materia.nome AS materia_nome,
-        materia.id AS materia_id
+//         materia.nome AS materia_nome,
+//         materia.id AS materia_id
 
-    FROM
-        sportello
-    INNER JOIN docente
-    ON sportello.docente_id = docente.id
-    INNER JOIN materia
-    ON sportello.materia_id = materia.id
-    WHERE sportello.id = '$sportello_id';";
+//     FROM
+//         sportello
+//     INNER JOIN docente
+//     ON sportello.docente_id = docente.id
+//     INNER JOIN materia
+//     ON sportello.materia_id = materia.id
+//     WHERE sportello.id = '$sportello_id';";
 
-$sportello = dbGetFirst($query);
+// $sportello = dbGetFirst($query);
 
-$mailbody = "Lo/a studente/ssa ".$__studente_cognome." ".$__studente_nome." ha prenotato lo sportello di ".$sportello['materia_nome']." previsto per il giorno " . $sportello['sportello_data'] . " alle ore " . $sportello['sportello_ora'] . " indicando il seguente argomento: " . $argomento;
-
+$mailbody =  "<html><body>Buongiorno ".$docente_nome." ".$docente_cognome.",<br> ricevi questa mail perchè lo sportello di ".$materia." previsto per il giorno " . $data . " alle ore " . $ora . " ha registrato il primo iscritto.<br>";
+$mailbody .= "Lo/a studente/ssa iscritto è ".$studente_cognome." ".$studente_nome." della classe ".$studente_classe." ed ha prenotato indicato il seguente argomento: " . $argomento;
+$mailbody .= "<br>";
+$mailbody .= "mail generata automaticamente da <b>GestOre</b><br></body></html>";
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= "From: " . $sender . "\r\n"."X-Mailer: php";
+info("mail inviata al docente");
+info("TESTO: ".$mailbody);
 $sender = $__settings->local->emailNoReplyFrom;
-$mailsubject = 'GestOre - Prenotazione sportello ' . $sportello['materia_nome'];
-mail($sportello['docente_email'], $mailsubject, $mailbody ,  additional_params: "-f$sender")
+$mailsubject = 'GestOre - Prenotazione sportello ' . $materia;
+mail($docente_email, $mailsubject, $mailbody ,  $headers, additional_params: "-f$sender")
 ?>
--- studente/sportelloIscriviStudente.php
+++ studente/sportelloIscriviStudente.php
@@ -25,6 +25,7 @@ if(isset($_POST)) {
 	$studente_classe = $_POST['studente_classe'];
 	$docente_cognome = $_POST['docente_cognome'];
 	$docente_nome = $_POST['docente_nome'];
+	$docente_email = $_POST['docente_email'];
 
 	dbExec("INSERT INTO sportello_studente(iscritto, argomento, sportello_id, studente_id) VALUES(true, '$argomento', $sportello_id, $__studente_id)");
 	$last_id = dblastId();
@@ -36,7 +37,17 @@ if(isset($_POST)) {
 		info("aggiornato sportello con il suo argomento sportello_id=$sportello_id argomento=$argomento");
 	}
 	$date_time = $data . " " . $ora . ":00";
-	$dateT=date_create($date_time , timezone_open("Europe/Oslo"));
+	$dateT=date_create($date_time , timezone_open("Europe/Rome"));
+	// Check Daylight Saving Time
+	$isDST = $dateT->format("I");	
+	if ($isDST)
+	{
+		$dateT->sub(new DateInterval('PT2H'));
+	}
+	else
+	{
+		$dateT->sub(new DateInterval('PT1H'));
+	}
 	$datetime_sportello = date_format($dateT,"Ymd-His");
 	$datetime_sportello = str_replace("-","T",$datetime_sportello);
 	$durata_minuti = $numero_ore * 50;
@@ -44,8 +55,20 @@ if(isset($_POST)) {
 	$dateT_fine->modify(' + ' . $durata_minuti . ' minutes');
 	$datetime_fine_sportello = date_format($dateT_fine,"Ymd-His");
 	$datetime_fine_sportello = str_replace("-","T",$datetime_fine_sportello);
-	info("formato Google inizio: " . $datetime_sportello . " fine: " . $datetime_fine_sportello . " luogo: " . $luogo);
-	require_once 'sportelloMail.php';
-
+	
+	info("PRIMA DI INVIO MAIL STUDENTE");
+	// invio la mail di iscrizione allo studente
+	require 'sportelloMailStudente.php';
+	info("DOPO INVIO MAIL STUDENTE");
+	$query = "	SELECT COUNT(*) FROM sportello_studente WHERE sportello_studente.sportello_id = " . $sportello_id;
+	
+	$numero_studenti_iscritti = dbGetValue($query);
+	info("NUMERO STUDENTI ISCRITTI ALLO SPORTELLO: ".$numero_studenti_iscritti);
+	// è il primo studente dello sportello?
+	if ($numero_studenti_iscritti == 1)
+	{
+		// invio una mail al docente per avvisarlo che c'è almeno uno studente iscritto
+		require 'sportelloInviaMailDocente.php';
+	}
 }
 ?>
