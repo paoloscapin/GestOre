a0ded51f2c39c7eeafc2e6377e3723f029621c29
Massimo Saiani <massimo.saiani@buonarroti.tn.it>
2024-10-12 17:49:09 +0200

    completato codice per cancellazione iscrizione studente a sportello

--- studente/sportelloCancellaIscrizione.php
+++ studente/sportelloCancellaIscrizione.php
@@ -13,19 +13,52 @@ require_once '../common/checkSession.php';
 if(isset($_POST['id']) && isset($_POST['id']) != "") {
 	$sportello_id = $_POST['id'];
 	$materia = $_POST['materia'];
+	$argomento = $_POST['argomento'];
+	$data = $_POST['data'];
+	$ora = $_POST['ora'];
+	$numero_ore = $_POST['numero_ore'];
+	$luogo = $_POST['luogo'];
+	$studente_cognome = $_POST['studente_cognome'];
+	$studente_nome = $_POST['studente_nome'];
+	$studente_email = $_POST['studente_email'];
+	$studente_classe = $_POST['studente_classe'];
+	$docente_cognome = $_POST['docente_cognome'];
+	$docente_nome = $_POST['docente_nome'];
+	$docente_email = $_POST['docente_email'];
 
+	$date_time = $data . " " . $ora . ":00";
+	$dateT=date_create($date_time , timezone_open("Europe/Rome"));
+	// Check Daylight Saving Time
+	$isDST = $dateT->format("I");	
+	if ($isDST)
+	{
+		$dateT->sub(new DateInterval('PT2H'));
+	}
+	else
+	{
+		$dateT->sub(new DateInterval('PT1H'));
+	}
+
+	// inverto format data - giorno con mese
+	$data_array = explode("-", $data);
+	$data = $data_array[2] . "-" . $data_array[1] . "-" . $data_array[0];
+	
 	dbExec("DELETE FROM sportello_studente WHERE sportello_id = $sportello_id AND studente_id  = $__studente_id");
 	info("cancellata iscrizione di $__studente_cognome $__studente_nome dallo sportello di $materia sportello_id=$sportello_id");
+	require 'sportelloMailCancellazioneStudente.php';
 
-	// se e' settato un solo argomento, nel caso si cancelli l'ultimo studente, non ha senso tenere l'argomento per cui toglie anche quello
-	if (getSettingsValue("sportelli", "unSoloArgomento", true)) {
+	$iscritti = dbGetValue("SELECT COUNT(*) FROM sportello_studente WHERE sportello_studente.sportello_id = $sportello_id;");
+	if ($iscritti == 0) 
+	{	// se e' settato un solo argomento, nel caso si cancelli l'ultimo studente, non ha senso tenere l'argomento per cui toglie anche quello
+	
+		if (getSettingsValue("sportelli", "unSoloArgomento", true)) {
 		// controlla quanti studenti sono ancora iscritti
-		$iscritti = dbGetValue("SELECT COUNT(*) FROM sportello_studente WHERE sportello_studente.sportello_id = $sportello_id;");
-		if ($iscritti == 0) {
+
 			debug("non ci sono altri iscritti quindi cancello l'argomento");
 			dbExec("UPDATE sportello SET argomento='' WHERE id = $sportello_id;");
 			info("cancellato argomento per lo sportello id=$sportello_id");
 		}
+		require 'sportelloInviaMailCancellazioneDocente.php';
 	}
 }
 ?>
---
+++ studente/sportelloInviaMailCancellazioneDocente.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ *  This file is part of GestOre
+ *  @author     Paolo Scapin <paolo.scapin@gmail.com>
+ *  @copyright  (C) 2018 Paolo Scapin
+ *  @license    GPL-3.0+ <https://www.gnu.org/licenses/gpl-3.0.html>
+ */
+
+
+ 
+require_once '../common/checkSession.php';
+ruoloRichiesto('studente','segreteria-didattica','dirigente');
+
+
+$full_mail_body = file_get_contents("template_mail_cancella_docente.html");
+
+
+$linkCalendar = 'https://calendar.google.com/calendar/render?action=TEMPLATE&dates=' . $datetime_sportello . 'Z%2F' . $datetime_fine_sportello . 'Z&details=' . urlencode("Sportello di " . $materia . ' - Aula ' . urlencode($luogo)) . '&location=' . urlencode('Istituto Tecnico Tecnologico Buonarroti, Via Brigata Acqui, 15, 38122 Trento TN, Italia') . '&text=' . urlencode("Sportello di " . $materia . " - Aula " . $luogo);
+
+//
+$full_mail_body = str_replace("{titolo}","ANNULLAMENTO SPORTELLO",$full_mail_body);
+$full_mail_body = str_replace("{nome}",strtoupper($docente_cognome) . " " . strtoupper($docente_nome),$full_mail_body);
+$full_mail_body = str_replace("{messaggio}","hai ricevuto questa mail perchè l'ultimo studente iscritto allo sportello si è iscritto. Quindi in mancanza di altre iscrizioni lo sportello è annullato",$full_mail_body);
+$full_mail_body = str_replace("{data}",$data,$full_mail_body);
+$full_mail_body = str_replace("{ora}",$ora,$full_mail_body);
+$full_mail_body = str_replace("{docente}",strtoupper($docente_cognome . " " . $docente_nome),$full_mail_body);
+$full_mail_body = str_replace("{materia}",$materia,$full_mail_body);
+$full_mail_body = str_replace("{aula}",$luogo,$full_mail_body);
+$full_mail_body = str_replace("{messaggio_finale}","Se dovesse iscriversi nuovamente uno studente sarai avvisato via mail.",$full_mail_body);
+
+$sender = $__settings->local->emailNoReplyFrom;
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= "From: " . $sender . "\r\n"."X-Mailer: php";
+$mailsubject = 'GestOre - Annullamento sportello ' . $materia;
+mail($docente_email, $mailsubject, $full_mail_body ,  $headers, additional_params: "-f$sender");
+info("inviata mail di cancellazione ultimo studente dallo sportello - email: " . $docente_email);
+?>
