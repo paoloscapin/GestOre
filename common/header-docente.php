0c23f475bc23d4fe5757e7102b446b6d5e188194
Paolo Scapin <paolo.scapin@archifit.com>
2024-10-24 15:19:42 +0200

    small fix header docente

--- common/header-docente.php
+++ common/header-docente.php
@@ -31,7 +31,11 @@
 
 <?php
 require_once '../common/connect.php';
-$num = dbGetValue("SELECT COUNT(id) FROM gruppo WHERE gruppo.dipartimento = false AND gruppo.anno_scolastico_id = $__anno_scolastico_corrente_id AND gruppo.responsabile_docente_id = $__docente_id;");
+if (empty($__docente_id)) {
+	$num = 1;
+} else {
+	$num = dbGetValue("SELECT COUNT(id) FROM gruppo WHERE gruppo.dipartimento = false AND gruppo.anno_scolastico_id = $__anno_scolastico_corrente_id AND gruppo.responsabile_docente_id = $__docente_id;");
+}
 if ($num > 0) {
 	echo '<a href="'.$__application_base_path.'/docente/gruppo.php" class="btn btn-default navbar-btn btn-lightblue4" role="button"><span class="glyphicon glyphicon-user"></span>&ensp;Gruppi </a>';
 }
