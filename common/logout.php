95becf1facc0444bbe3e8b90ac738cf57097eb6b
Massimo Saiani ITT Buonarroti <57176050+MassimoSaianiBuonarroti@users.noreply.github.com>
2025-09-06 18:30:53 +0200

    corretto logout in base al ruolo impersonato

--- common/logout.php
+++ common/logout.php
@@ -12,14 +12,88 @@ require_once __DIR__ . '/__Settings.php';
 require_once __DIR__ . '/path.php';
 require_once __DIR__ . '/__Log.php';
 require_once __DIR__ . '/__Util.php';
+require_once '../common/checkSession.php';
+
+$__redirectURL = $__http_base_link . '/index.php';
+
+if (haRuolo('admin'))
+{
+    debug("Ha ruolo Admin");
+}
+if (impersonaRuolo('docente'))
+{
+    debug("Impersona docente");
+}
+if (impersonaRuolo('studente'))
+{
+    debug("Impersona studente");
+}
+if (impersonaRuolo('genitore'))
+{
+    debug("Impersona genitore");
+}
+debug("Ruolo attuale: $__utente_ruolo");
+debug("Username attuale: $__username");
+debug("Docente ID attuale: $__docente_id");
+debug("Studente ID attuale: $__studente_id");
+debug("Genitore ID attuale: $__genitore_id");
+
+if (haRuolo('admin')) {
+    // verifico se sto impersonando qualcun altro:
+    if (impersonaRuolo(('docente'))) 
+        {
+        // se stavo impersonando un docente, torno alla mia sessione
+        unset($_SESSION['docente_id']);
+        unset($_SESSION['docente_nome']);
+        unset($_SESSION['docente_cognome']);
+        unset($_SESSION['docente_email']);
+        $__docente_id = -1;
+        $__docente_nome = '';
+        $__docente_cognome = '';
+        $__docente_email = '';
+        info("Torno alla sessione da docente ad admin");
+        redirect('/index.php');
+    }
+    if (impersonaRuolo(('studente'))) 
+        {
+        // se stavo impersonando un studente, torno alla mia sessione
+        unset($_SESSION['studente_id']);
+        unset($_SESSION['studente_nome']);
+        unset($_SESSION['studente_cognome']);
+        unset($_SESSION['studente_email']);
+        unset($_SESSION['studente_codice_fiscale']);
+        $__studente_id = -1;
+        $__studente_nome = '';
+        $__studente_cognome = '';
+        $__studente_email = '';
+        $__studente_codice_fiscale = '';
+
+        info("Torno alla sessione da studente ad admin");
+        redirect('/index.php');
+    }
+    if (impersonaRuolo(('genitore'))) 
+        {
+        // se stavo impersonando un genitore, torno alla mia sessione
+        unset($_SESSION['genitore_id']);
+        unset($_SESSION['genitore_nome']);
+        unset($_SESSION['genitore_cognome']);
+        unset($_SESSION['genitore_email']);
+        unset($_SESSION['genitore_codice_fiscale']);
+        $__genitore_id = -1;
+        $__genitore_nome = '';
+        $__genitore_cognome = '';
+        $__genitore_email = '';
+        $__genitore_codice_fiscale = '';
+        info("Torno alla sessione da genitore ad admin");
+        redirect('/index.php');
+    }
+}
 
 // start session
 if (session_status() == PHP_SESSION_NONE) {
 	session_set_cookie_params ($__settings->system->durata_sessione);
 	session_start();
 }
-
-$__redirectURL = $__http_base_link . '/index.php';
 
 //Include Google client library
 require_once __DIR__ . '/google-client-library/src/Google_Client.php';
