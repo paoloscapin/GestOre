0b17ff3e79a29ca94402831fbd5271e8b55ba207
Massimo Saiani ITT Buonarroti <57176050+MassimoSaianiBuonarroti@users.noreply.github.com>
2025-09-24 16:00:50 +0200

    Corretto bug nome docente con apostrofo

--- segreteria/oreAssegnate.php
+++ segreteria/oreAssegnate.php
@@ -11,91 +11,92 @@ require_once '../common/checkSession.php';
 
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Ore Assegnate</title>
+	<title>Ore Assegnate</title>
 	<meta charset="UTF-8">
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-<?php
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<?php
 
 
-require_once '../common/header-common.php';
-require_once '../common/style.php';
-require_once '../common/_include_bootstrap-select.php';
-require_once '../common/_include_select2.php';
-ruoloRichiesto('dirigente','segreteria-docenti');
-?>
+	require_once '../common/header-common.php';
+	require_once '../common/style.php';
+	require_once '../common/_include_bootstrap-select.php';
+	require_once '../common/_include_select2.php';
+	ruoloRichiesto('dirigente', 'segreteria-docenti');
+	?>
 
-<link rel="stylesheet" href="<?php echo $__application_base_path; ?>/css/table-green-3.css">
-<link rel="stylesheet" href="<?php echo $__application_base_path; ?>/css/header-style.css">
+	<link rel="stylesheet" href="<?php echo $__application_base_path; ?>/css/table-green-3.css">
+	<link rel="stylesheet" href="<?php echo $__application_base_path; ?>/css/header-style.css">
 
-<!-- Custom JS file -->
-<script type="text/javascript" src="js/oreAssegnate.js"></script>
+	<!-- Custom JS file -->
+	<script type="text/javascript" src="js/oreAssegnate.js"></script>
 </head>
 
-<body >
-<?php require_once '../common/header-segreteria.php'; ?>
+<body>
+	<?php require_once '../common/header-segreteria.php'; ?>
 
-<!-- Content Section -->
-<div class="container-fluid" style="margin-top:60px">
-<div class="panel panel-lima4">
-<div class="panel-heading container-fluid">
-	<div class="row">
-		<div class="col-md-3">
-			<span class="glyphicon glyphicon-list-alt"></span>&emsp;Ore Assegnate
+	<!-- Content Section -->
+	<div class="container-fluid" style="margin-top:60px">
+		<div class="panel panel-lima4">
+			<div class="panel-heading container-fluid">
+				<div class="row">
+					<div class="col-md-3">
+						<span class="glyphicon glyphicon-list-alt"></span>&emsp;Ore Assegnate
+					</div>
+					<div class="col-md-3 text-center">
+						<label id="import_btn" class="btn btn-xs btn-lima4 btn-file"><span class="glyphicon glyphicon-upload"></span>&emsp;Importa<input type="file" id="file_select_id" style="display: none;"></label>
+					</div>
+					<div class="col-md-3 text-center">
+						<button onclick="ricalcolaTutti()" class="btn btn-xs btn-lima4"><span class="glyphicon glyphicon-refresh"></span>&ensp;Ricalcola Tutti</button>
+					</div>
+					<div class="col-md-4 text-right">
+					</div>
+					<div class="col-md-4 text-right">
+					</div>
+				</div>
+			</div>
 		</div>
-        <div class="col-md-3 text-center">
-            <label id="import_btn" class="btn btn-xs btn-lima4 btn-file"><span class="glyphicon glyphicon-upload"></span>&emsp;Importa<input type="file" id="file_select_id" style="display: none;"></label>
-        </div>
-		<div class="col-md-3 text-center">
-			<button onclick="ricalcolaTutti()" class="btn btn-xs btn-lima4"><span class="glyphicon glyphicon-refresh"></span>&ensp;Ricalcola Tutti</button>
+		<div class="row" style="margin-bottom:10px;">
+			<div class="col-md-12 text-center" id='result_text'>
+			</div>
 		</div>
-		<div class="col-md-4 text-right">
-		</div>
-		<div class="col-md-4 text-right">
-		</div>
-	</div>
-</div>
-</div>
-<div class="row"  style="margin-bottom:10px;">
-	<div class="col-md-12 text-center" id='result_text'>
-	</div>
-</div>
-<div class="row">
-<div class="col-md-12">
+		<div class="row">
+			<div class="col-md-12">
 
 
-<?php
-// prepara l'elenco delle categorie di attivita'
-$query = "	SELECT * FROM `ore_previste_tipo_attivita` WHERE valido = true AND inserito_da_docente = false AND previsto_da_docente = false;";
-$resultArrayTipoAttivita = dbGetAll($query);
+				<?php
+				// prepara l'elenco delle categorie di attivita'
+				$query = "	SELECT * FROM `ore_previste_tipo_attivita` WHERE valido = true AND inserito_da_docente = false AND previsto_da_docente = false;";
+				$resultArrayTipoAttivita = dbGetAll($query);
 
-$data = '';
-foreach($resultArrayTipoAttivita as $tipoAttivita) {
-	$tipoAttivitaId = $tipoAttivita['id'];
-	$categoria = $tipoAttivita['categoria'];
-	$nome = $tipoAttivita['nome'];
-	$ore = $tipoAttivita['ore'];
-	$ore_max = $tipoAttivita['ore_max'];
-	$da_rendicontare = $tipoAttivita['da_rendicontare'];
-	$data .= '
+				$data = '';
+				foreach ($resultArrayTipoAttivita as $tipoAttivita) {
+					$tipoAttivitaId = $tipoAttivita['id'];
+					$categoria = $tipoAttivita['categoria'];
+					$nome = $tipoAttivita['nome'];
+					$ore = $tipoAttivita['ore'];
+					$ore_max = $tipoAttivita['ore_max'];
+					$da_rendicontare = $tipoAttivita['da_rendicontare'];
+					$data .= '
 		<div class="panel panel-lima4">
 		<div class="panel-heading container-fluid">
 			<div class="row">
 				<div class="col-md-4">
 				</div>
 				<div class="col-md-4 text-center">
-					<strong>'.$nome.'</strong>
+					<strong>' . $nome . '</strong>
 				</div>
 				<div class="col-md-4 text-right">
-					<button onclick="addAttivita('.$tipoAttivitaId.',\''.addslashes($nome).'\','.$ore.','.$ore_max.')" class="btn btn-xs btn-info"><span class="glyphicon glyphicon-plus"></span></button>
+					<button onclick="addAttivita(' . $tipoAttivitaId . ',\'' . addslashes($nome) . '\',' . $ore . ',' . $ore_max . ')" class="btn btn-xs btn-info"><span class="glyphicon glyphicon-plus"></span></button>
 				</div>
 			</div>
 		</div>
 		<div class="panel-body">
 		';
-	$data .= '
+					$data .= '
 		<div class="table-wrapper">
-			<table class="table table-bordered table-striped" id="table_'.$tipoAttivitaId.'" >
+			<table class="table table-bordered table-striped" id="table_' . $tipoAttivitaId . '" >
 				<thead>
 					<th style="display:none;">id</th>
 					<th class="col-md-2">Docente</th>
@@ -106,7 +107,7 @@ foreach($resultArrayTipoAttivita as $tipoAttivita) {
 				<tbody>
 			';
 
-	$query = "	SELECT
+					$query = "	SELECT
 					ore_previste_attivita.id AS ore_previste_attivita_id,
 					ore_previste_attivita.dettaglio AS ore_previste_attivita_dettaglio,
 					ore_previste_attivita.ore AS ore_previste_attivita_ore,
@@ -126,155 +127,170 @@ foreach($resultArrayTipoAttivita as $tipoAttivita) {
 					docente.nome ASC
 				;
 		";
-		$resultArrayOre = dbGetAll($query);
-		$classname = "";
-		foreach($resultArrayOre as $row_ore) {
-			$classname = ($classname==="even_row") ? "odd_row" : "even_row";
-			$data .= '
-							<tr class="'.$classname.'">
-								<td style="display:none;">'.$row_ore['ore_previste_attivita_id'].'</td>
-								<td>'.$row_ore['docente_cognome'].' '.$row_ore['docente_nome'].'</td>
-								<td>'.$row_ore['ore_previste_attivita_dettaglio'].'</td>
-								<td class="col-md-1 text-center">'.$row_ore['ore_previste_attivita_ore'].'</td>
-                                <td class="col-md-2 text-center">
-                                    <button onclick="attivitaGetDetails ('.$row_ore['ore_previste_attivita_id'].','.$row_ore['ore_previste_attivita_ore_previste_tipo_attivita_id'].')" class="btn btn-warning btn-xs"><span class="glyphicon glyphicon-pencil"></button>
+					$resultArrayOre = dbGetAll($query);
+					$classname = "";
+					foreach ($resultArrayOre as $row_ore) {
+						$classname = ($classname === "even_row") ? "odd_row" : "even_row";
+						$data .= '
+    <tr class="'.$classname.'">
+        <td style="display:none;">'.$row_ore['ore_previste_attivita_id'].'</td>
+        <td>'.$row_ore['docente_cognome'].' '.$row_ore['docente_nome'].'</td>
+        <td>'.$row_ore['ore_previste_attivita_dettaglio'].'</td>
+        <td class="col-md-1 text-center">'.$row_ore['ore_previste_attivita_ore'].'</td>
+        <td class="col-md-2 text-center">
+            <button onclick="attivitaGetDetails('
+                .$row_ore['ore_previste_attivita_id'].','.$row_ore['ore_previste_attivita_ore_previste_tipo_attivita_id'].
+            ')" class="btn btn-warning btn-xs">
+                <span class="glyphicon glyphicon-pencil"></span>
+            </button>
 
-									<botton onclick="deleteOreAttivita('.$row_ore['ore_previste_attivita_id'].','.$row_ore['ore_previste_attivita_ore_previste_tipo_attivita_id'].',\''.addslashes($row_ore['docente_cognome']).'\',\''.addslashes($row_ore['docente_nome']).'\')" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></botton>
-								</td>
-							</tr>
-					';
-		}
-		$data .= '
+            <button onclick="deleteOreAttivita('
+                .$row_ore['ore_previste_attivita_id'].',' 
+                .$row_ore['ore_previste_attivita_ore_previste_tipo_attivita_id'].',\' ' 
+                .str_replace('\'',' ',$row_ore['docente_cognome']).'\',\'' 
+                .str_replace('\'',' ',$row_ore['docente_nome']).'\')" 
+                class="btn btn-danger btn-xs">
+                <span class="glyphicon glyphicon-trash"></span>
+            </button>
+        </td>
+    </tr>';
+
+
+
+							
+					}
+					$data .= '
 				</tbody>
 				';
-	$data .= '
+					$data .= '
 			</table>
 			<div style="page-break-after: always;">
 		</div>
 	</div>
 ';
-	$data .= '
+					$data .= '
   </div> <!-- // Div - panel body -->
 </div> <!-- // Div - inner panel -->
 ';
-}
-echo $data;
-?>
+				}
+				echo $data;
+				?>
 
-</div>
-</div>
-</div> <!-- // Div - Content Section Container-fluid -->
+			</div>
+		</div>
+	</div> <!-- // Div - Content Section Container-fluid -->
 
-<?php
+	<?php
 
-// prepara l'elenco dei docenti
-$docenteOptionList = '				<option value="0"></option>';
-$query = "	SELECT * FROM docente
+	// prepara l'elenco dei docenti
+	$docenteOptionList = '				<option value="0"></option>';
+	$query = "	SELECT * FROM docente
 			WHERE docente.attivo = true
 			ORDER BY docente.cognome, docente.nome ASC
 			;";
-$resultArray = dbGetAll($query);
-foreach($resultArray as $row) {
-	$docenteOptionList .= '
-		<option value="'.$row['id'].'" >'.$row['cognome'].' '.$row['nome'].'</option>
+	$resultArray = dbGetAll($query);
+	foreach ($resultArray as $row) {
+		$docenteOptionList .= '
+		<option value="' . $row['id'] . '" >' . $row['cognome'] . ' ' . $row['nome'] . '</option>
 	';
-}
-?>
+	}
+	?>
 
-<!-- Bootstrap Modals -->
-<!-- Modal - Add New Record -->
-<div class="modal fade" id="add_new_record_modal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-body">
-			<div class="panel panel-lima4">
-			<div class="panel-heading">
-				<h5 class="modal-title" id="myModalLabel">Nuovo Assegnamento</h5>
+	<!-- Bootstrap Modals -->
+	<!-- Modal - Add New Record -->
+	<div class="modal fade" id="add_new_record_modal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div class="modal-dialog modal-lg" role="document">
+			<div class="modal-content">
+				<div class="modal-body">
+					<div class="panel panel-lima4">
+						<div class="panel-heading">
+							<h5 class="modal-title" id="myModalLabel">Nuovo Assegnamento</h5>
+						</div>
+						<div class="panel-body">
+							<form class="form-horizontal">
+
+								<div class="form-group docente_incaricato_selector">
+									<label class="col-sm-2 control-label" for="docente_incaricato">Docente</label>
+									<div class="col-sm-8">
+										<select id="docente_incaricato" name="docente_incaricato" class="docente_incaricato selectpicker" data-style="btn-success" data-live-search="true"
+											data-noneSelectedText="seleziona..." data-width="70%">
+											<?php echo $docenteOptionList ?>
+										</select>
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label class="col-sm-2 control-label" for="dettaglio">Dettaglio</label>
+									<div class="col-sm-8"><input type="text" id="dettaglio" placeholder="dettaglio" class="form-control" /></div>
+								</div>
+
+								<div class="form-group">
+									<label class="col-sm-2 control-label" for="ore" id="oreLabel">Ore</label>
+									<div class="col-sm-8"><input type="text" id="ore" placeholder="ore" class="form-control" /></div>
+								</div>
+							</form>
+
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">Annulla</button>
+							<button type="button" class="btn btn-primary" onclick="oreAssegnateAddRecord()">Salva</button>
+							<input type="hidden" id="hidden_ore_previste_tipo_attivita_id">
+							<input type="hidden" id="hidden_ore_previste_attivita_id">
+						</div>
+					</div>
+				</div>
 			</div>
-			<div class="panel-body">
-			<form class="form-horizontal">
+		</div>
+	</div> <!-- // Div - add_new_record_modal -->
+	<!-- // Modal - Add New Record -->
 
-                <div class="form-group docente_incaricato_selector">
-                    <label class="col-sm-2 control-label" for="docente_incaricato">Docente</label>
-					<div class="col-sm-8">
-                        <select id="docente_incaricato" name="docente_incaricato" class="docente_incaricato selectpicker" data-style="btn-success" data-live-search="true"
-					data-noneSelectedText="seleziona..." data-width="70%" >
-                            <?php echo $docenteOptionList ?>
-					    </select>
-                    </div>
-                </div>
+	<!-- Modal - Update attivita details -->
+	<div class="modal fade" id="update_attivita_modal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="myModalProfiloLabel">
+		<div class="modal-dialog modal-lg" role="document">
+			<div class="modal-content">
+				<div class="modal-body">
+					<div class="panel panel-lightblue4">
+						<div class="panel-heading">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+							<h5 class="modal-title" id="myModalProfiloLabel">Attività</h5>
+						</div>
+						<div class="panel-body">
 
-                <div class="form-group">
-                    <label class="col-sm-2 control-label" for="dettaglio">Dettaglio</label>
-                    <div class="col-sm-8"><input type="text" id="dettaglio" placeholder="dettaglio" class="form-control"/></div>
-                </div>
+							<div class="form-group">
+								<label for="update_docente_incaricato">Docente</label>
+								<input type="text" id="update_docente_incaricato" placeholder="docente" class="form-control" disabled />
+							</div>
 
-                <div class="form-group">
-                    <label class="col-sm-2 control-label" for="ore" id="oreLabel" >Ore</label>
-                    <div class="col-sm-8"><input type="text" id="ore" placeholder="ore" class="form-control"/></div>
-                </div>
-			</form>
+							<div class="form-group">
+								<label for="update_attivita">Attività</label>
+								<input type="text" id="update_attivita" placeholder="attività" class="form-control" disabled />
+							</div>
 
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Annulla</button>
-                <button type="button" class="btn btn-primary" onclick="oreAssegnateAddRecord()">Salva</button>
-				<input type="hidden" id="hidden_ore_previste_tipo_attivita_id">
-				<input type="hidden" id="hidden_ore_previste_attivita_id">
-            </div>
+							<div class="form-group">
+								<label for="update_dettaglio">Dettaglio</label>
+								<input type="text" id="update_dettaglio" placeholder="dettaglio" class="form-control" />
+							</div>
+
+							<div class="form-group">
+								<label for="update_ore">Ore</label>
+								<input type="text" id="update_ore" placeholder="ore" class="form-control" />
+							</div>
+
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">Annulla</button>
+							<button type="button" class="btn btn-primary" onclick="attivitaUpdateDetails()">Salva</button>
+							<input type="hidden" id="hidden_ore_previste_attivita_id">
+							<input type="hidden" id="hidden_ore_previste_tipo_attivita_id">
+						</div>
+					</div>
+				</div>
 			</div>
-			</div>
-        </div>
-    </div>
-</div> <!-- // Div - add_new_record_modal -->
-<!-- // Modal - Add New Record -->
+		</div>
+	</div>
 
-<!-- Modal - Update attivita details -->
-<div class="modal fade" id="update_attivita_modal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="myModalProfiloLabel">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-body">
-			<div class="panel panel-lightblue4">
-			<div class="panel-heading">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h5 class="modal-title" id="myModalProfiloLabel">Attività</h5>
-            </div>
-            <div class="panel-body">
-
-                <div class="form-group">
-                    <label for="update_docente_incaricato">Docente</label>
-                    <input type="text" id="update_docente_incaricato" placeholder="docente" class="form-control" disabled />
-                </div>
-
-                <div class="form-group">
-                    <label for="update_attivita">Attività</label>
-                    <input type="text" id="update_attivita" placeholder="attività" class="form-control" disabled />
-                </div>
-
-                <div class="form-group">
-                    <label for="update_dettaglio">Dettaglio</label>
-                    <input type="text" id="update_dettaglio" placeholder="dettaglio" class="form-control"/>
-                </div>
-
-                <div class="form-group">
-                    <label for="update_ore">Ore</label>
-                    <input type="text" id="update_ore" placeholder="ore" class="form-control"/>
-                </div>
-
-            </div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">Annulla</button>
-				<button type="button" class="btn btn-primary" onclick="attivitaUpdateDetails()" >Salva</button>
-				<input type="hidden" id="hidden_ore_previste_attivita_id">
-				<input type="hidden" id="hidden_ore_previste_tipo_attivita_id">
-            </div>
-			</div>
-			</div>
-        </div>
-    </div>
-</div>
-
-<!-- // Modal - Update docente details -->
+	<!-- // Modal - Update docente details -->
 
 </body>
+
 </html>
