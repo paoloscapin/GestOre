ae6722d42780c71457ff756782690b0b37857bd8
Massimo Saiani ITT Buonarroti <57176050+MassimoSaianiBuonarroti@users.noreply.github.com>
2026-01-10 12:06:12 +0100

    fix: rimosso checksession non necessario

--- common/logout.php
+++ common/logout.php
@@ -11,7 +11,6 @@ require_once __DIR__ . '/__Settings.php';
 require_once __DIR__ . '/path.php';
 require_once __DIR__ . '/__Log.php';
 require_once __DIR__ . '/__Util.php';
-require_once __DIR__ . '/checkSession.php';
 
 $base = $_GET['base'] ?? '';
 
