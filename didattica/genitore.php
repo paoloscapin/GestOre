a2230f1e9a41273356b043502d365898d85c2304
Massimo Saiani ITT Buonarroti <57176050+MassimoSaianiBuonarroti@users.noreply.github.com>
2026-01-10 12:10:46 +0100

    fix: corretta posizione ruolorichiesto per evitare blank page

--- didattica/genitore.php
+++ didattica/genitore.php
@@ -7,6 +7,7 @@
  *  @license    GPL-3.0+ <https://www.gnu.org/licenses/gpl-3.0.html>
  */
 require_once '../common/checkSession.php';
+ruoloRichiesto('segreteria-didattica', 'dirigente');
 ?>
 
 <!DOCTYPE html>
@@ -20,7 +21,6 @@ require_once '../common/checkSession.php';
     require_once '../common/_include_bootstrap-toggle.php';
     require_once '../common/_include_bootstrap-select.php';
     require_once '../common/_include_flatpickr.php';
-    ruoloRichiesto('segreteria-didattica', 'dirigente');
     ?>
     <link rel="stylesheet" href="<?php echo $__application_base_path; ?>/css/table-green-2.css">
     <title>Genitori</title>
