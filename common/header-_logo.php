69d1fb9b88ebdd2cf72c7644e9c0ded09db107c5
Massimo Saiani ITT Buonarroti <57176050+MassimoSaianiBuonarroti@users.noreply.github.com>
2026-04-12 16:32:24 +0200

    fix: aggiornata visibilita versione come tooltip

--- common/header-_logo.php
+++ common/header-_logo.php
@@ -10,7 +10,7 @@
 ?>
 
 <?php
-if(file_exists(__DIR__ . '/../version.php')) {
+if (file_exists(__DIR__ . '/../version.php')) {
 	include_once __DIR__ . '/../version.php';
 } else {
 	$__software_name = APPLICATION_NAME;
@@ -23,11 +23,20 @@ $changeLogLinkEnd = '</a>';
 
 ?>
 
-	<div class="navbar-header">
-		<div class="releaseversion"><?php echo $changeLogLinkBegin;?>&nbsp;
-			<span class="releaseversiontext"><p><strong><?php echo $__software_name;?></strong></p><hr><p>Version: <?php echo $__software_version;?></p><p>Release date: <?php echo $__software_release_date;?></p></span><?php echo $changeLogLinkEnd;?></div>
-			<a href="../index.php" class="navbar-brand top-navbar-brand" >
-				<img style="height: 44px; margin-top: -10px;" src="data:image/png;base64,<?php echo base64_encode(dbGetValue("SELECT src FROM immagine WHERE nome = 'logo.png'")); ?>" alt="Logo">
-			</a>
-			<a class="navbar-brand top-navbar-brand" href="#"> </a>
+<div class="navbar-header">
+
+	<div class="logo-tooltip-wrapper">
+		<a href="../index.php" class="navbar-brand top-navbar-brand">
+			<img style="height: 44px; margin-top: -10px;"
+			     src="data:image/png;base64,<?php echo base64_encode(dbGetValue("SELECT src FROM immagine WHERE nome = 'logo.png'")); ?>"
+			     alt="Logo">
+		</a>
+
+		<div class="release-tooltip">
+			<strong><?php echo $__software_name; ?></strong><br>
+			v<?php echo $__software_version; ?><br>
+			<?php echo $__software_release_date; ?>
+		</div>
 	</div>
+
+</div>
--- version.php
+++ version.php
@@ -7,6 +7,6 @@
  *  @license    GPL-3.0+ <https://www.gnu.org/licenses/gpl-3.0.html>
  */
 $__software_name = 'GestOre';
-$__software_version = '1.2.243';
+$__software_version = '1.2.244';
 $__software_release_date = '12 apr 2026';
 ?>
