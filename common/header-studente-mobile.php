a55d2be11d0b87843bb04e10b141bfd40595709b
Massimo Saiani ITT Buonarroti <57176050+MassimoSaianiBuonarroti@users.noreply.github.com>
2026-04-29 18:42:43 +0200

    fix: corretta visibilità pulsante biglietti da telefono lato studenti

--- common/header-studente-mobile.php
+++ common/header-studente-mobile.php
@@ -50,6 +50,10 @@
                 }
                 ?>
 
+                <li><a class="btn btn-yellow4" href="../common/biglietti_prenotazioni.php">
+                        <span class="glyphicon glyphicon-barcode"></span> Biglietti
+                    </a></li>
+
                 <?php
                 if (getSettingsValue('config', 'carenzeObiettiviMinimi', false) && getSettingsValue('carenzeObiettiviMinimi', 'visibile_studenti', false)) : ?>
                     <li><a class="btn btn-lightblue4" href="../studente/carenze_mobile.php">
