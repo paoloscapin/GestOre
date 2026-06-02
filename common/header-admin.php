1602db1df20e06c1b6e9a8ada583f53c8b1e9e83
Massimo Saiani ITT Buonarroti <57176050+MassimoSaianiBuonarroti@users.noreply.github.com>
2026-06-02 17:29:29 +0200

    fix: aggiunto orario builder nel menu admin

--- common/header-admin.php
+++ common/header-admin.php
@@ -12,7 +12,7 @@
 
 <nav class="navbar navbar-default navbar-fixed-top top-navbar top-navbar-default">
 	<div class="container-fluid">
-	<?php require_once __DIR__ . '/header-_logo.php'; ?>
+		<?php require_once __DIR__ . '/header-_logo.php'; ?>
 
 		<ul class="nav navbar-nav top-navbar-nav admin-navbar-nav">
 
@@ -79,16 +79,29 @@
 					<li><a href="<?php echo $__application_base_path; ?>/admin/mastercom_l2_registro.php"><span class="glyphicon glyphicon-check"></span>&ensp;L2 - Registro</a></li>
 				</ul>
 			</li>
+			<li class="dropdown">
+				<a href="#" class="btn btn-default btn-purple4 nav-btn dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<span class="glyphicon glyphicon-calendar"></span>&ensp;Orario scolastico <span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+					<li><a href="<?php echo $__application_base_path; ?>/orario_builder/index.php"><span class="glyphicon glyphicon-dashboard"></span>&ensp;Dashboard</a></li>
+					<li><a href="<?php echo $__application_base_path; ?>/orario_builder/scenari.php"><span class="glyphicon glyphicon-duplicate"></span>&ensp;Scenari</a></li>
+					<li><a href="<?php echo $__application_base_path; ?>/orario_builder/piani_orario.php"><span class="glyphicon glyphicon-list-alt"></span>&ensp;Piani orario</a></li>
+					<li><a href="<?php echo $__application_base_path; ?>/orario_builder/aule.php"><span class="glyphicon glyphicon-blackboard"></span>&ensp;Aule</a></li>
+					<li><a href="<?php echo $__application_base_path; ?>/orario_builder/alternanze.php"><span class="glyphicon glyphicon-transfer"></span>&ensp;Alternanze materie</a></li>			
+					<li><a href="<?php echo $__application_base_path; ?>/orario_builder/aula_gruppi.php"><span class="glyphicon glyphicon-blackboard"></span>&ensp;Gruppi aule</a></li>	
+				</ul>
+			</li>
 			<li><a href="<?php echo $__application_base_path; ?>/orario/orario.php" class="btn btn-default btn-lightblue4 nav-btn" role="button"><span class="glyphicon glyphicon-time"></span>&ensp;Orario </a></li>
 
 		</ul>
 		<ul class="nav navbar-nav navbar-right top-navbar-nav admin-navbar-nav-right">
-		<li><a href="<?php echo $__application_base_path; ?>/help/GestOre - Guida Studenti.pdf" target="_blank" ><span class="glyphicon glyphicon-bishop"></span></a></li>
-		<li><a href="<?php echo $__settings->local->helpLinkDocente; ?>" target="_blank" ><span class="glyphicon glyphicon-education"></span></a></li>
-		<li><a href="<?php echo $__settings->local->helpLinkSegreteria; ?>" target="_blank" ><span class="glyphicon glyphicon-tower"></span></a></li>
-		<li><a href="<?php echo $__settings->local->helpLinkDirigente; ?>" target="_blank" ><span class="glyphicon glyphicon-king"></span></a></li>
-		<li><a href="<?php echo $__settings->local->helpLinkAdmin; ?>" target="_blank" ><span class="glyphicon glyphicon-text-color"></span></a></li>
-			<li><a><span class=""></span><?php echo $__utente_nome.' '.$__utente_cognome ?></a></li>
+			<li><a href="<?php echo $__application_base_path; ?>/help/GestOre - Guida Studenti.pdf" target="_blank"><span class="glyphicon glyphicon-bishop"></span></a></li>
+			<li><a href="<?php echo $__settings->local->helpLinkDocente; ?>" target="_blank"><span class="glyphicon glyphicon-education"></span></a></li>
+			<li><a href="<?php echo $__settings->local->helpLinkSegreteria; ?>" target="_blank"><span class="glyphicon glyphicon-tower"></span></a></li>
+			<li><a href="<?php echo $__settings->local->helpLinkDirigente; ?>" target="_blank"><span class="glyphicon glyphicon-king"></span></a></li>
+			<li><a href="<?php echo $__settings->local->helpLinkAdmin; ?>" target="_blank"><span class="glyphicon glyphicon-text-color"></span></a></li>
+			<li><a><span class=""></span><?php echo $__utente_nome . ' ' . $__utente_cognome ?></a></li>
 			<li><a href="<?php echo $__application_base_path . '/common/logout.php?base=admin' ?>"><span class="glyphicon glyphicon-log-out"></span></a></li>
 		</ul>
 	</div>
