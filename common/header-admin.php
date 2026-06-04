c63c71a54a1f1cef930e312a87e3cfb43212352c
Massimo Saiani ITT Buonarroti <57176050+MassimoSaianiBuonarroti@users.noreply.github.com>
2026-06-04 18:49:51 +0200

    fix: aggiunto menu lato admin pianifica corsi carenze

--- common/header-admin.php
+++ common/header-admin.php
@@ -69,6 +69,7 @@
 					<li><a href="<?php echo $__application_base_path; ?>/admin/mastercom_grades.php"><span class="glyphicon glyphicon-list-alt"></span>&ensp;Voti</a></li>
 					<li><a href="<?php echo $__application_base_path; ?>/admin/mastercom_grade_insert.php"><span class="glyphicon glyphicon-pencil"></span>&ensp;Inserisci voti</a></li>
 					<li><a href="<?php echo $__application_base_path; ?>/admin/mastercom_debts.php"><span class="glyphicon glyphicon-alert"></span>&ensp;Carenze</a></li>
+					<li><a href="<?php echo $__application_base_path; ?>/admin/mastercom_debts_plan.php"><span class="glyphicon glyphicon-calendar"></span>&ensp;Pianifica carenze</a></li>
 					<li role="separator" class="divider"></li>
 					<li class="dropdown-header">Registri speciali</li>
 					<li><a href="<?php echo $__application_base_path; ?>/admin/mastercom_noirc.php"><span class="glyphicon glyphicon-book"></span>&ensp;NO IRC - Configurazione</a></li>
